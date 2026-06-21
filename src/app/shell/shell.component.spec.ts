37f8847f082caaa58fd86ff47fe90acd408da349
Rrothschild18 <raultonell18@gmail.com>
2026-06-20 21:54:19 -0300

    Initial commit

---
+++ src/app/shell/shell.component.spec.ts
@@ -0,0 +1,30 @@
+import { waitForAsync, ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { ShellComponent } from './shell.component';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatListModule } from '@angular/material/list';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatToolbarModule } from '@angular/material/toolbar';
+
+describe('ShellComponent', () => {
+  let component: ShellComponent;
+  let fixture: ComponentFixture<ShellComponent>;
+
+  beforeEach(waitForAsync(() => {
+    TestBed.configureTestingModule({
+      declarations: [ShellComponent],
+      imports: [MatButtonModule, MatIconModule, MatListModule, MatSidenavModule, MatToolbarModule],
+    });
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(ShellComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should compile', () => {
+    expect(component).toBeTruthy();
+  });
+});
